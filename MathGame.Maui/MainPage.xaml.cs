7d3d23dc9eb50cd440d2d29fefc35d2797f7891b
TheCSharpAcademy <thecsharpacademy@gmail.com>
2022-08-07 17:46:39 +1000

    Menu Created

--- MathGame.Maui/MainPage.xaml.cs
+++ MathGame.Maui/MainPage.xaml.cs
@@ -1,7 +1,13 @@
-﻿namespace MathGame.Maui;
+﻿using System.Collections.ObjectModel;
+using Windows.Gaming.Preview.GamesEnumeration;
+
+namespace MathGame.Maui;
 
 public partial class MainPage : ContentPage
 {
-	
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 }
 
