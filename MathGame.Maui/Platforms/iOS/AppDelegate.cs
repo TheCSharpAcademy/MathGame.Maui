a168590d07e5731a2002fb612e9b122d321d4417
TheCSharpAcademy <thecsharpacademy@gmail.com>
2022-08-07 11:22:44 +1000

    cleaning up

--- MathGame.Maui/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MathGame.Maui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
