9486ef84327a91f4d69d78566d872e1198502a38
TheCSharpAcademy <thecsharpacademy@gmail.com>
2022-08-11 22:18:09 +1000

    Creating questions

--- MathGame.Maui/GamePage.xaml.cs
+++ MathGame.Maui/GamePage.xaml.cs
@@ -3,11 +3,48 @@ namespace MathGame.Maui;
 public partial class GamePage : ContentPage
 {
     public string GameType { get; set; }
+    int firstNumber = 0;
+    int secondNumber = 0;
 
     public GamePage(string gameType)
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
         GameType = gameType;
         BindingContext = this;
+
+        CreateNewQuestion();
+    }
+
+    private void CreateNewQuestion()
+    {
+        var gameOperand = GameType switch
+        {
+            "Addition" => "+",
+            "Subtraction" => "-",
+            "Multiplication" => "*",
+            "Division" => "/",
+            _ => ""
+        };
+
+        var random = new Random();
+
+        firstNumber = GameType != "Division" ? random.Next(1, 9) : random.Next(1, 99);
+        secondNumber = GameType != "Division" ? random.Next(1, 9) : random.Next(1, 99);
+
+        if (GameType == "Division")
+        {
+            while (firstNumber < secondNumber || firstNumber % secondNumber != 0)
+            {
+                firstNumber = random.Next(1, 99);
+                secondNumber = random.Next(1, 99);
+            }
+        }
+
+        QuestionLabel.Text = $"{firstNumber} {gameOperand} {secondNumber}";
+
+    }
+
+    private void OnAnswerSubmitted(object sender, EventArgs e)
+    {
     }
 }
