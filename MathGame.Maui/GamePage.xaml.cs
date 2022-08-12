f9a4eb0dc534ce0abbd13e828d995a9d97c5e046
paguirre82 <pablod@teambaw.com.au>
2022-08-12 11:14:30 +1000

    Background and fonts

--- MathGame.Maui/GamePage.xaml.cs
+++ MathGame.Maui/GamePage.xaml.cs
@@ -20,15 +20,6 @@ public partial class GamePage : ContentPage
 
     private void CreateNewQuestion()
     {
-        var gameOperand = GameType switch
-        {
-            "Addition" => "+",
-            "Subtraction" => "-",
-            "Multiplication" => "*",
-            "Division" => "/",
-            _ => ""
-        };
-
         var random = new Random();
 
         firstNumber = GameType != "Division" ? random.Next(1, 9) : random.Next(1, 99);
@@ -43,7 +34,7 @@ public partial class GamePage : ContentPage
             }
         }
 
-        QuestionLabel.Text = $"{firstNumber} {gameOperand} {secondNumber}";
+        QuestionLabel.Text = $"{firstNumber} {GameType} {secondNumber}";
 
     }
 
@@ -54,19 +45,19 @@ public partial class GamePage : ContentPage
 
         switch (GameType)
         {
-            case "Addition" :
+            case "+" :
                 isCorrect = answer == firstNumber + secondNumber;
                 ProcessAnswer(isCorrect);
                 break;
-            case "Subtraction":
+            case "-":
                 isCorrect = answer == firstNumber - secondNumber;
                 ProcessAnswer(isCorrect);
                 break;
-            case "Multiplication":
+            case "×":
                 isCorrect = answer == firstNumber * secondNumber;
                 ProcessAnswer(isCorrect);
                 break;
-            case "Division":
+            case "÷":
                 isCorrect = answer == firstNumber / secondNumber;
                 ProcessAnswer(isCorrect);
                 break;
