f5b7dd60aa53252b00fd68618f803d44198181a3
paguirre82 <pablod@teambaw.com.au>
2022-08-12 14:04:20 +1000

    Caveat Brush

--- MathGame.Maui/MauiProgram.cs
+++ MathGame.Maui/MauiProgram.cs
@@ -12,6 +12,7 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 fonts.AddFont("Roboto-Bold.ttf", "RobotoBold");
+				fonts.AddFont("CaveatBrush-Regular.ttf", "CaveatBrush");
             });
 
 		return builder.Build();
